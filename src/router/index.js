0e1c50fecfb08ef95fda963f95c7a7137e934f77
vdsou <vdsouza@outlook.com>
2022-05-10 14:50:11 -0300

    :lipstick: add settings styles

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Playlists from "../views/Playlists.vue";
+import Settings from "../views/Settings.vue";
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: "/playlists",
     name: "Playlists",
     component: Playlists,
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    component: Settings,
   },
   {
     path: "/about",
---
+++ src/views/Settings.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="settings">
+    <div class="settings-title">
+      <h1>Configurações</h1>
+      <i><img src="../assets/edit.svg" alt="Editar" /></i>
+    </div>
+    <form action="" class="settings-form">
+      <div class="update-username">
+        <label for="username">Alterar usuário</label>
+        <input
+          type="text"
+          id="username"
+          placeholder="insira aqui seu novo usuário"
+        />
+      </div>
+      <div class="update-password">
+        <label for="password">Alterar senha</label>
+        <input
+          type="text"
+          id="password"
+          placeholder="insira aqui sua nova senha "
+        />
+        <input
+          type="text"
+          id="password"
+          placeholder="repita aqui sua nova senha "
+        />
+      </div>
+      <button type="submit">Salvar</button>
+    </form>
+    <button class="delete-account" type="button">Excluir minha conta</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Settings",
+};
+</script>
+
+<style scoped>
+.settings {
+  margin: 3.5rem;
+}
+.settings .settings-title {
+  display: flex;
+  align-items: center;
+}
+.settings-form {
+  display: flex;
+  height: 40rem;
+  flex-direction: column;
+  justify-content: space-around;
+}
+.settings-form input {
+  width: 24rem;
+  height: 3rem;
+  border-radius: 0.5rem;
+  text-align: center;
+  font-size: 1.2rem;
+  margin: 0.6rem 0;
+  color: #000;
+}
+.settings-form label {
+  font-weight: bold;
+  font-size: 1.6rem;
+}
+.settings-form input::placeholder {
+  color: var(--grey-color);
+}
+.settings-form .update-username,
+.settings-form .update-password {
+  display: flex;
+  flex-direction: column;
+}
+.settings-form button {
+  width: 7rem;
+  height: 3.5rem;
+  font-weight: bold;
+  background: #079992;
+  border-radius: 0.5rem;
+  cursor: pointer;
+}
+.delete-account {
+  background: var(--danger-color);
+  padding: 0.8rem 1.2rem;
+  font-size: 1.3rem;
+  font-weight: 500;
+  border-radius: 0.5rem;
+  cursor: pointer;
+}
+</style>
