6682e49862903baaab69b624ee68692640dbf8ed
vdsou <vdsouza@outlook.com>
2022-05-11 16:09:47 -0300

    :lipstick: add home link to nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -9,6 +9,9 @@
       </div>
       <ul>
         <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
           <router-link to="/settings">Editar Perfil</router-link>
         </li>
         <li>
